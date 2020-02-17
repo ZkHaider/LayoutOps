350c30fc7c7057bf21780bcf1befa9a3952d167c
ZkHaider <heathbbb7@gmail.com>
2020-02-16 22:50:44 -0800

    Added CoreGraphics imports

--- Sources/Layout.Align.End.swift
+++ Sources/Layout.Align.End.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreGraphics
 
 public struct AlignEnd {
     let insets: LXEdgeInsets
--- Sources/Layout.HCenter.swift
+++ Sources/Layout.HCenter.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreGraphics
 
 public struct HCenter {
     let insets: LXEdgeInsets
